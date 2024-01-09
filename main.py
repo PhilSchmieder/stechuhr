33e4ff01e8a4c1bac4cb2e3a100b9b3974cdb8ac
Phil Schmieder <phil.schmieder@laptop>
2024-01-09 12:01:03 +0100

    migrate CLI to click

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import argparse
 import datetime
 import shutil
 import sqlite3
 from os import remove, mkdir
 from os.path import exists, join
+
+import click
 import jinja2
 from jinja2 import FileSystemLoader
 
@@ -22,8 +23,8 @@ DELETE_ALL = "delete from time"
 DELETE_BY_ID = "delete from time where id = ?"
 
 
-def run_query(db_file, query, obj=None):
-    with sqlite3.connect(db_file) as con:
+def run_query(database, query, obj=None):
+    with sqlite3.connect(database) as con:
         cur = con.cursor()
 
         if obj:
@@ -36,69 +37,89 @@ def run_query(db_file, query, obj=None):
         return cur.fetchall()
 
 
-def clock_in(db_file, time: str):
+@click.group(help="Stechuhr is a CLI tool to keep time of your working hours.")
+def cli():
+    pass
+
+
+@cli.command(name="in", help="Clock in.")
+@click.option("-d", "--database", help="Database file. Defaults to 'stechuhr.db'.", default="stechuhr.db")
+@click.option("-t", "--time", help="Clock in with given time. Format: YYYY-MM-DD HH:mm:ss. Defaults to now.")
+def clock_in(database, time: str):
     time = time if time else datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    return run_query(db_file, INSERT_CLOCK_IN, (time,))
+    return run_query(database, INSERT_CLOCK_IN, (time,))
 
 
-def clock_out(db_file, time: str):
+@cli.command(name="out", help="Clock out.")
+@click.option("-d", "--database", help="Database file. Defaults to 'stechuhr.db'.", default="stechuhr.db")
+@click.option("-t", "--time", help="Clock out with given time. Format: YYYY-MM-DD HH:mm:ss. Defaults to now.")
+def clock_out(database, time: str):
     time = time if time else datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    (identifier, in_time, out_time) = run_query(db_file, GET_LATEST_ENTRY)[0]
+    (identifier, in_time, out_time) = run_query(database, GET_LATEST_ENTRY)[0]
 
     if out_time:
-        print("WARNING: The latest entry already has a clock out time.")
-        print("Did you forget to clock in?.")
-        print("A new entry will be created with only a clock out time.")
+        click.echo("WARNING: The latest entry already has a clock out time.")
+        click.echo("Did you forget to clock in?.")
+        click.echo("A new entry will be created with only a clock out time.")
 
-        return run_query(db_file, INSERT_CLOCK_OUT, (time,))
+        return run_query(database, INSERT_CLOCK_OUT, (time,))
 
     t = (
         in_time,
         time,
         identifier
     )
-    return run_query(db_file, UPDATE_CLOCK_IN_OUT, t)
+    return run_query(database, UPDATE_CLOCK_IN_OUT, t)
 
 
-def print_db(db_file):
-    pprint(run_query(db_file, SELECT_ALL))
+@cli.command(name="print", help="Print Stechuhr data.")
+@click.option("-d", "--database", help="Database file. Defaults to 'stechuhr.db'.", default="stechuhr.db")
+def print_db(database):
+    pprint(run_query(database, SELECT_ALL))
 
 
 def pprint(lines: list[(int, datetime, datetime)]) -> None:
-    print("ID\tIn\t\t\tOut")
+    click.echo("ID\tIn\t\t\tOut")
     for (identifier, in_time, out_time) in lines:
-        print(f"{identifier}\t{in_time}\t{out_time}")
+        click.echo(f"{identifier}\t{in_time}\t{out_time}")
 
 
-def update_entry(db_file, identifier, in_time, out_time):
+@cli.command(name="update", help="Update entry with id IDENTIFIER.")
+@click.argument("identifier", type=int, required=True)
+@click.option("-d", "--database", help="Database file. Defaults to 'stechuhr.db'.", default="stechuhr.db")
+@click.option("-i", "--in-time", help="Clock in with given time. Format: YYYY-MM-DD HH:mm:ss")
+@click.option("-o", "--out-time", help="Clock out with given time. Format: YYYY-MM-DD HH:mm:ss")
+def update_entry(database, identifier, in_time, out_time):
     if not identifier:
-        print("Specify which entry to update using --identifier ID.")
+        click.echo("Specify which entry to update using --identifier ID.")
         return
 
     if in_time:
-        run_query(db_file, UPDATE_CLOCK_IN, (in_time, identifier))
+        run_query(database, UPDATE_CLOCK_IN, (in_time, identifier))
 
     if out_time:
-        run_query(db_file, UPDATE_CLOCK_OUT, (out_time, identifier))
+        run_query(database, UPDATE_CLOCK_OUT, (out_time, identifier))
 
 
-def new_db(db_file):
-    if exists(db_file):
-        c = input(f"File {db_file} already exists. Do you want to overwrite it? [y/N]: ")
+@cli.command(name="new", help="Create new Stechuhr.")
+@click.option("-d", "--database", help="Database file. Defaults to 'stechuhr.db'.", default="stechuhr.db")
+def new_db(database):
+    if exists(database):
+        c = input(f"File {database} already exists. Do you want to overwrite it? [y/N]: ")
         if c.lower() == "y":
-            remove(db_file)
+            remove(database)
         else:
             return
 
-    open(db_file, "w").close()
-    run_query(db_file, CREATE_TABLE_TIME)
+    open(database, "w").close()
+    run_query(database, CREATE_TABLE_TIME)
 
 
-def _build_csv(db_file) -> str:
+def _build_csv(database) -> str:
     s = "id,in,out\n"
-    for (identifier, in_time, out_time) in run_query(db_file, SELECT_ALL):
+    for (identifier, in_time, out_time) in run_query(database, SELECT_ALL):
         s += f"{identifier},{in_time},{out_time}\n"
 
     return s
@@ -110,8 +131,8 @@ def _render_jinja_template(template_path: str, data: dict) -> str:
     return template.render(data)
 
 
-def _build_html(db_file, template_path):
-    entries = run_query(db_file, SELECT_ALL)
+def _build_html(database, template_path):
+    entries = run_query(database, SELECT_ALL)
 
     template_data = {
         "entries": [
@@ -128,22 +149,33 @@ def _build_html(db_file, template_path):
     return html
 
 
-def export(db_file, export_format: str, export_out: str, template_path: str):
+@cli.command(name="export", help="Export data.")
+@click.option("-d", "--database", help="Database file. Defaults to 'stechuhr.db'.", default="stechuhr.db")
+@click.option("-f", "--format", "export_format", help="Export format.", default="csv",
+              type=click.Choice(['CSV', 'HTML'], case_sensitive=False))
+@click.option("-o", "--out", help="Output path. Defaults to stdout.")
+@click.option("-t", "--template",
+              help="Path to jinja template for html export. Defaults to './templates/default.jinja'",
+              default="./templates/default.jinja")
+def export(database, export_format: str, out: str, template: str):
     export_data = None
 
     if export_format.lower() == "csv":
-        export_data = _build_csv(db_file)
+        export_data = _build_csv(database)
 
     elif export_format.lower() == "html":
-        export_data = _build_html(db_file, template_path)
+        export_data = _build_html(database, template)
 
-    if export_out:
-        with open(export_out, "w") as f:
+    if out:
+        with open(out, "w") as f:
             f.write(export_data)
     else:
-        print(export_data)
+        click.echo(export_data)
 
 
+@cli.command(name="archive", help="Archive Stechuhr.")
+@click.option("-d", "--database", help="Database file. Defaults to 'stechuhr.db'.", default="stechuhr.db")
+@click.option("--archive-dir", help="Path to archive directory. Defaults to './archive/'", default="./archive/")
 def archive(database, archive_dir):
     if not exists(archive_dir):
         mkdir(archive_dir)
@@ -152,89 +184,68 @@ def archive(database, archive_dir):
     shutil.copy(database, str(join(archive_dir, timestamp + "_" + database)))
 
 
-def reset(db_file, no_interact: bool = False):
+@cli.command(name="reset", help="Reset Stechuhr.")
+@click.option("-d", "--database", help="Database file. Defaults to 'stechuhr.db'.", default="stechuhr.db")
+def reset(database, no_interact: bool = False):
     if no_interact:
-        run_query(db_file, DELETE_ALL)
+        run_query(database, DELETE_ALL)
         return
 
-    c = input(f"Are you sure you want to reset {db_file}? [y/N]: ")
+    c = input(f"Are you sure you want to reset {database}? [y/N]: ")
     if c.lower() == "y":
-        run_query(db_file, DELETE_ALL)
+        run_query(database, DELETE_ALL)
 
 
-def delete_entry(db_file, identifier):
+@cli.command(name="delete", help="Delete Stechuhr entry.")
+@click.argument("identifier", type=int, required=True)
+@click.option("-d", "--database", help="Database file. Defaults to 'stechuhr.db'.", default="stechuhr.db")
+def delete_entry(database, identifier):
     if not identifier:
-        print("Specify which entry to delete using --identifier ID.")
+        click.echo("Specify which entry to delete using --identifier ID.")
         return
 
-    run_query(db_file, DELETE_BY_ID, (identifier,))
+    run_query(database, DELETE_BY_ID, (identifier,))
 
 
-def create_parser():
-    parser = argparse.ArgumentParser(prog='Stechuhr', description='Keep time of your working hours')
-
-    parser.add_argument("action",
-                        choices=["new", "in", "out", "print", "update", "delete", "export", "archive", "reset",
-                                 "merge", "sort"], nargs="?", default="print")
-    parser.add_argument("--in-time", help="Clock in with given time. Format: YYYY-MM-DD HH:mm:ss")
-    parser.add_argument("--out-time", help="Clock out with given time. Format: YYYY-MM-DD HH:mm:ss")
-    parser.add_argument("--identifier", help="ID of the entry you want to update/delete.", type=int)
-    parser.add_argument("--database", help="Specify database file.", default="stechuhr.db")
-    parser.add_argument("--to-merge", help="Path to database to merge.")
-    parser.add_argument("--archive-dir", help="Directory to store archived databases.", default="./archive/")
-    parser.add_argument("--export-format", help="Format to export database in.", nargs="?", choices=["CSV", "HTML"],
-                        default="CSV")
-    parser.add_argument("--export-file", help="Path to export to.")
-    parser.add_argument("--template-path", help="Path Jinja template for HTML export.",
-                        default="./templates/default.jinja")
-
-    return parser
-
-
-def merge(db_file, to_merge):
+@cli.command(name="merge", help="Merge Stechuhr with Stechuhr TO_MERGE.")
+@click.argument("TO_MERGE", type=str, required=True)
+@click.option("-d", "--database", help="Database file. Defaults to 'stechuhr.db'.", default="stechuhr.db")
+def merge(database, to_merge):
     for (_, in_time, out_time) in run_query(to_merge, SELECT_ALL):
         if in_time:
-            clock_in(db_file, in_time)
+            clock_in(database, in_time)
 
         if out_time:
-            clock_out(db_file, out_time)
+            clock_out(database, out_time)
 
 
-def sort(db_file):
-    ordered_entries = run_query(db_file, SELECT_ALL_ORDERED_TIMEWISE)
-    run_query(db_file, DELETE_ALL)
+@cli.command(name="sort", help="Sort Stechuhr entries by clock in time.")
+@click.option("-d", "--database", help="Database file. Defaults to 'stechuhr.db'.", default="stechuhr.db")
+def sort(database):
+    ordered_entries = run_query(database, SELECT_ALL_ORDERED_TIMEWISE)
+    run_query(database, DELETE_ALL)
 
     for (_, in_time, out_time) in ordered_entries:
         if in_time:
-            clock_in(db_file, in_time)
+            clock_in(database, in_time)
 
         if out_time:
-            clock_out(db_file, out_time)
+            clock_out(database, out_time)
+
+
+def recursive_help(cmd, parent=None):
+    ctx = click.core.Context(cmd, info_name=cmd.name, parent=parent)
+    print(cmd.get_help(ctx))
+    print()
+    commands = getattr(cmd, 'commands', {})
+    for sub in commands.values():
+        recursive_help(sub, ctx)
+
+
+@cli.command(hidden=True)
+def dumphelp():
+    recursive_help(cli)
 
 
 if __name__ == '__main__':
-    parser = create_parser()
-    args = parser.parse_args()
-
-    if args.action == "new":
-        new_db(args.database)
-    elif args.action == "in":
-        clock_in(args.database, args.in_time)
-    elif args.action == "out":
-        clock_out(args.database, args.out_time)
-    elif args.action == "print":
-        print_db(args.database)
-    elif args.action == "update":
-        update_entry(args.database, args.identifier, args.in_time, args.out_time)
-    elif args.action == "delete":
-        delete_entry(args.database, args.identifier)
-    elif args.action == "export":
-        export(args.database, args.export_format, args.export_file, args.template_path)
-    elif args.action == "archive":
-        archive(args.database, args.archive_dir)
-    elif args.action == "reset":
-        reset(args.database)
-    elif args.action == "merge":
-        merge(args.database, args.to_merge)
-    elif args.action == "sort":
-        sort(args.database)
+    cli()
